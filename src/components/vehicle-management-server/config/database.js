ca85238c5caf3144a1093f24a2c815d5dbee402b
AkashMedishetty <97078698+AkashMedishetty@users.noreply.github.com>
2024-11-20 20:43:40 +0530

    Update database.js
    added the porting to the hosting service

--- src/components/vehicle-management-server/config/database.js
+++ src/components/vehicle-management-server/config/database.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'vehicle_management',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 module.exports = pool.promise();
