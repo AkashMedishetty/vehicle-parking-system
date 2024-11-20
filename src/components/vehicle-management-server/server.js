761858c0fff4bfbc98d99e056aeb2a47f36a4832
AkashMedishetty <97078698+AkashMedishetty@users.noreply.github.com>
2024-11-20 16:09:08 +0000

    Replace cors to use vercel backend

--- src/components/vehicle-management-server/server.js
+++ src/components/vehicle-management-server/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({
-    origin: 'https://vehicle-parking-system-gh5yhr57y-akashmedishettys-projects.vercel.app',
+    origin: 'https://vehicle-parking-system-mshzdrecp-akashmedishettys-projects.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
