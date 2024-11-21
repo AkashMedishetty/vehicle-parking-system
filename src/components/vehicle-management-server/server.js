24ee48dd978fc1cf48427ec1cd884988cfacd0ae
AkashMedishetty <97078698+AkashMedishetty@users.noreply.github.com>
2024-11-21 08:55:46 +0000

    Updatd server.js and package.json

--- src/components/vehicle-management-server/server.js
+++ src/components/vehicle-management-server/server.js
@@ -18,6 +18,10 @@ const hashPassword = (password) => {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
 // Middleware setup
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
