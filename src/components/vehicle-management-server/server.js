dd2a3a5cbbd542b7f365046c98cd541945e71816
AkashMedishetty <97078698+AkashMedishetty@users.noreply.github.com>
2024-12-05 10:34:55 +0530

    Update server.js with PORT to port and using port and adding the console.log and HTTP/2

--- src/components/vehicle-management-server/server.js
+++ src/components/vehicle-management-server/server.js
@@ -16,7 +16,7 @@ const hashPassword = (password) => {
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
